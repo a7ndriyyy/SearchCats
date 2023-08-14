ae06189d2aea91d2bbcda92e6e8c81116bb5255d
a7ndriyyy <andrii.nastych@gmail.com>
2023-08-14 20:10:07 +0200

    done

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import SlimSelect from 'slim-select';
+import { fetchBreeds, fetchCatByBreed } from './JS/cat-api';
+import refs from './JS/refs';
