6e257ad61ac825feee8e113f12a9e6e745fa72c4
a7ndriyyy <andrii.nastych@gmail.com>
2023-08-14 20:40:37 +0200

    done

--- src/JS/cat-api.js
+++ src/JS/cat-api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-axios.defaults.headers.common["x-api-key"] = 
-"тlive_gIIwgFyWslEu2Y6NSYnnI4PrvIv2DcwMVKksrVA3CLZ7CHFtp9ealtdYK2B7Qt0n";
+axios.defaults.headers.common[`x-api-key`] = 
+`тlive_gIIwgFyWslEu2Y6NSYnnI4PrvIv2DcwMVKksrVA3CLZ7CHFtp9ealtdYK2B7Qt0n`;
 const API_URL = 'https://api.thecatapi.com/v1/breeds';
 const SEARCH_URL = `https://api.thecatapi.com/v1/images/search`;
 function fetchBreeds() {
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 import SlimSelect from 'slim-select';
-import { fetchBreeds, fetchCatByBreed } from './JS/cat-api';
-import refs from './JS/refs';
+// import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
+import refs from './js/refs';
+
+refs.selectEl.addEventListener('change', onValueId);
+fetchBreeds()
+  .then(arr => {
+    load();
+
+    return (refs.selectEl.innerHTML = createMarkup(arr.data));
+  })
+  .then(() => slim())
+  .catch(fetchError);
