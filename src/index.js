35919a2774dd0b0c52ee44aa39d41d8b0bd397ec
a7ndriyyy <andrii.nastych@gmail.com>
2023-08-15 13:48:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Report } from 'notiflix/build/notiflix-report-aio';
 import SlimSelect from 'slim-select';
-// import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
+import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 import refs from './js/refs';
 
-refs.selectEl.addEventListener('change', onValueId);
+refs.selectEl.addEventListener('change', onValue);
 fetchBreeds()
   .then(arr => {
     load();
@@ -13,3 +13,31 @@ fetchBreeds()
   })
   .then(() => slim())
   .catch(fetchError);
+
+  function onValue(evt) {
+    const id = evt.target.value;
+    fetchCatByBreed(id)
+      .then(obj => {
+        load();
+  
+        return (refs.catInfoEl.innerHTML = createMarkupCat(obj.data));
+      })
+      .then(() => success())
+      .catch(fetchError);
+  }
+  function fetchError() {
+    Report.failure(refs.error.textContent, '');
+  }
+  function success() {
+    Notify.success('Search was successful!)', '');
+  }
+  function load() {
+    refs.selectEl.hidden = false;
+    refs.loaderEl.classList.remove('loader');
+  }
+  
+  function slim() {
+    new SlimSelect({
+      select: refs.selectEl,
+    });
+  }
